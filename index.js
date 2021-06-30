ba5e5b5b37daef0b913540705adc19f51ec0aa55
Quinn Lonergan <qui99lo@gmail.com>
2021-06-29 21:18:48 -0400

    commit tree 14f0921017b844e8097bb501371f2a74013012c0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//function superbowlWin(record){
+    //record.result.find(wins => {
+   //     wins === "W"
+   // })
+ //   return record.year
+//}
+
+function superbowlWin(record) {
+    const win = (record.find(game => game.result === "W" 
+        ))
+        if (win){
+            return win.year;
+        } else return undefined
+}
 
